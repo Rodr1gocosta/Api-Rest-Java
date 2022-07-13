b655a18ebd2b90a99bbc847b98326c20e70884b2
Rodr1gocosta <rodrigomcosta20@gmail.com>
2022-07-13 00:26:50 -0300

    API REST FINAL 1

--- src/main/java/br/com/cursojava/cursojava/controller/form/AtualizacaoTopicoForm.java
+++ src/main/java/br/com/cursojava/cursojava/controller/form/AtualizacaoTopicoForm.java
@@ -5,45 +5,37 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
-import br.com.cursojava.cursojava.modelo.Curso;
 import br.com.cursojava.cursojava.modelo.Topico;
-import br.com.cursojava.cursojava.repository.CursoRepository;
+import br.com.cursojava.cursojava.repository.TopicoRepository;
 
-public class TopicoForm {
+public class AtualizacaoTopicoForm {
 	
 	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
 	
 	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
-	
-	@NotNull @NotEmpty
-	private String nomeCurso;
-	
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getMensagem() {
 		return mensagem;
 	}
+
 	public void setMensagem(String mensagem) {
 		this.mensagem = mensagem;
 	}
-	public String getNomeCurso() {
-		return nomeCurso;
-	}
-	public void setNomeCurso(String nomeCurso) {
-		this.nomeCurso = nomeCurso;
-	}
-	public Topico converter(CursoRepository repository) {
-		Curso curso = repository.findByNome(nomeCurso);
-		return new Topico(titulo, mensagem, curso);
-	}
-	
-	
 
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getReferenceById(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		return topico;
+	}
 }
